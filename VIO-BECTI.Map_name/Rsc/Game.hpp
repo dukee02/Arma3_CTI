797a0cc4838c5caab864aa9c75fe38e3ecc1b4a4
dukee02 <68999242+dukee02@users.noreply.github.com>
2022-12-21 11:32:52 +0100

    Fixed: Town flag positions

--- VIO-BECTI.Map_name/Rsc/Game.hpp
+++ VIO-BECTI.Map_name/Rsc/Game.hpp
@@ -6,7 +6,7 @@ respawnDialog = false;
 //--- Require briefing.html to show up.
 onLoadName = "VIO-BECTI V2.0";
 onLoadMissionTime = false;
-onLoadMission = "VIO-BECTI V2.0_221216";
+onLoadMission = "VIO-BECTI V2.0_221221";
 
 briefingName = "VIO-BECTI V2.0";
 
