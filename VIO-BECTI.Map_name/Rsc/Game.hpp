593f4ae00ae3ad6753839795c2ac1051b6273928
dukee02 <68999242+dukee02@users.noreply.github.com>
2024-06-26 14:41:03 +0200

    Fixed: Error picture not found

--- VIO-BECTI.Map_name/Rsc/Game.hpp
+++ VIO-BECTI.Map_name/Rsc/Game.hpp
@@ -6,14 +6,17 @@ respawnDialog = false;
 //--- Require briefing.html to show up.
 onLoadName = "VIO-BECTI V2.0.4";
 onLoadMissionTime = false;
-onLoadMission = "VIO-BECTI V2.0.4_beta240621";
-
+overviewText = "VIO-BECTI V2.0.4";
+overviewTextLocked = "VIO-BECTI V2.0.4";
+onLoadMission = "VIO-BECTI V2.0.4_240626";
 briefingName = "VIO-BECTI V2.0.4";
 
 //--- ArmA 3 Specifics
-dev = "Benny";
-author = "Benny modified by dukee";
-overviewPicture = "VIO_BECTI.jpg";
+dev = "dukee";
+author = "dukee, basics by Benny";
+
+overViewPicture = "VIO_BECTI.jpg";
+overViewPictureLocked = "VIO_BECTI.jpg";
 loadScreen = "VIO_BECTI.jpg";
 
 //--- Properties.
