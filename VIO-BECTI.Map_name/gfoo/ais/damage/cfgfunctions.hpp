82b0668f8d5827002d0568f68b0f0d2853dee128
Gamerfoo <pascal.gomez0@gmail.com>
2024-10-18 23:38:18 +0200

    Add files via upload
    ais damage folder

---
+++ VIO-BECTI.Map_name/gfoo/ais/damage/cfgfunctions.hpp
@@ -0,0 +1,17 @@
+/*---chemin modifié---
+ajout de la racine gfoo\ 
+dans la commande de sous-classe: "file="
+*/
+
+class AIS_Damage
+{
+    class AIS_Damage
+    {
+        file = "gfoo\AIS\Damage";
+		class exitDamageHandler;
+		class getHitIndexValue;
+		class goToDead;
+		class handleDamage;
+		class verifyDamageType;
+    };
+};
